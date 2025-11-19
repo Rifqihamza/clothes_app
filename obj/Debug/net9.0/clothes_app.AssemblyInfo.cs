d00b3311769afff4196bec8cc30084f01dfb20da
Coders <rifqihamza30@gmail.com>
2025-11-19 17:14:05 +0700

    Add AssemblyInfo

--- obj/Debug/net9.0/clothes_app.AssemblyInfo.cs
+++ obj/Debug/net9.0/clothes_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clothes_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15993eea850379e4fd0f07aaf3d25860d7d9def1")]
 [assembly: System.Reflection.AssemblyProductAttribute("clothes_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clothes_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
